470be4aedfdd2219a0060beef6b25037cbf26358
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-17 17:31:07 +0100

    updates to pages and general styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Menu } from '../../components/Navigation/Menu/Menu'
+import { Hero } from '../../components/Hero/Hero'
+import { ShowcaseData } from '../../components/Showcase/ShowcaseData'
+import { QuotesData } from '../../components/Quotes/QuotesData'
+import { BestSellerData } from '../../components/BestSellers/BestSellerData'
+import { NewsLetter } from '../../components/NewsLetter/NewsLetter'
 
 export const Home = () => {
     return (
     <div>
-      <Menu />       
+      <Menu />
+      <Hero /> 
+      <ShowcaseData />  
+      <QuotesData />
+      <BestSellerData />  
+      <NewsLetter />  
     </div>
     )
 }
