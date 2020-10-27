04c4ff06827e24c354f219e41d51dcce7a52d121
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-27 02:52:11 +0000

    minor updates and redux provider setup to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
