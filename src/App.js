9ac089ba535e9b8cc9fbf7b195883a00a45bf77e
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-28 02:52:44 +0000

    changed navbar export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { NavBar } from '../src/components/Navigation/NavBar/NavBar'
+import NavBar from '../src/components/Navigation/NavBar/NavBar'
 
 const App = () => {
   return (
